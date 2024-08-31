37f03ed4226348674cdfe67d4c4b354452482cd7
Risheek <shadowalker1426@gmail.com>
2024-08-31 20:35:24 +0530

    s

--- setupServer.py
+++ setupServer.py
@@ -23,22 +23,17 @@ time.sleep(5)  # Wait for ngrok to start and be available
 # Update a file with the ngrok URL
 file_path = "src/routes/ngrok.txt"  # Path to the ngrok.txt file within your repo
 
-try:
-    while True:
-        ngrok_url = get_ngrok_url()
-        with open(file_path, "w") as file:
-            file.write(f"{ngrok_url}\n")
+ngrok_url = get_ngrok_url()
+with open(file_path, "w") as file:
+    file.write(f"{ngrok_url}\n")
 
-        # Add, commit, and push the changes to Git
-        repo_dir = "E:/Hackathon/Volvo Hackathon/Deployable"  # Change this to your local repository path
-        repo = git.Repo(repo_dir)
-        repo.git.add(file_path)
-        repo.index.commit(f"Update ngrok URL to {ngrok_url}")
-        origin = repo.remote(name='origin')
-        origin.push()
+# Add, commit, and push the changes to Git
+repo_dir = "E:/Hackathon/Volvo Hackathon/Deployable"  # Change this to your local repository path
+repo = git.Repo(repo_dir)
+repo.git.add(file_path)
+repo.index.commit(f"Update ngrok URL to {ngrok_url}")
+origin = repo.remote(name='origin')
+origin.push()
 
-        print(f"Ngrok URL ({ngrok_url}) saved to {file_path} and changes pushed to Git.")
-        time.sleep(60)  # Check every 60 seconds
+print(f"Ngrok URL ({ngrok_url}) saved to {file_path} and changes pushed to Git.")
 
-except KeyboardInterrupt:
-    print("Stopping script.")
